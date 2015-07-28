ff3a18074ba3c643867ddba6359619df1bb14f40
J. García <jyo.garcia@yahoo.com>
2015-07-27 23:52:51 -0600

    fix style

--- scripts/sync_from_gentoo.sh
+++ scripts/sync_from_gentoo.sh
@@ -13,47 +13,42 @@ else
     exit 1
 fi
 
-function sync_with_gentoo() {
-
-for package in  $(cat scripts/from_gentoo.txt) ; do
-    local forked_on_funtoo=0
-
-    if [[ -d $FUNTOO_REPO/$package || -d $GNOME_REPO/$package ]] ; then
-        forked_on_funtoo=1;
-        einfo "$package forked on funtoo";
-    else
-        remove_synced_package $package;
-        continue;
-    fi
-
-    sync_package $package
-done
-
+sync_with_gentoo() {
+    for package in  $(cat scripts/from_gentoo.txt) ; do
+        local forked_on_funtoo=0
+        if [[ -d $FUNTOO_REPO/$package || -d $GNOME_REPO/$package ]] ; then
+            forked_on_funtoo=1;
+            einfo "$package forked on funtoo";
+        else
+            remove_synced_package $package;
+            continue;
+        fi
+        sync_package $package
+    done
 }
 # Checks differneces and syncs if necessary
-function sync_package (){
+sync_package (){
     einfo " syncing package $1"
     local package=$1
-	local first_commit=0
+    local first_commit=0
     if [[ ! -d ./$package ]] ; then 
-		einfo "Creating directory for unsynced package: $package"
-		mkdir -p ./$package 
-		first_commit=1
-	fi
+        einfo "Creating directory for unsynced package: $package"
+        mkdir -p ./$package 
+        first_commit=1
+    fi
     diff -r -q -x ChangeLog -x Manifest "$GENTOO_REPO/$package" "$PWD/$package" 
     if [[ $? -gt 0 ]] ; then 
         einfo "Rsyncing $package"
         rsync  --exclude=Manifest --exclude=ChangeLog -v -a  "$GENTOO_REPO/$package/" "$(pwd)/$package/"
         einfo "Regenerating Manifest and commiting changes"
         pushd $package
-        #rm -rf ./ChangeLog ./Manifest
         sudo repoman manifest
-		git add ./*
-		if [[ $first_commit = 1 ]] ; then 
-			git commit -m  "$package: use gentoo's package" ;
-		else
-			git commit -m  "$package: update from gentoo's package" ;
-		fi
+        git add ./*
+        if [[ $first_commit = 1 ]] ; then 
+            git commit -m  "$package: use gentoo's package" ;
+        else
+            git commit -m  "$package: update from gentoo's package" ;
+        fi
         popd
         einfo "Sucess!!  $package"
     else
@@ -62,8 +57,19 @@ function sync_package (){
 
 }
 
-function remove_synced_package (){
+remove_synced_package (){
     ewarn " You should remove $1 seems not forked on funtoo"
 }
 
+update_repos(){
+    pushd $FUNTOO_REPO
+    git fetch && git pull
+    popd
+    pushd $GNOME_REPO
+    git fetch && git pull
+    popd
+    sudo emerge --sync
+}
+
 sync_with_gentoo
+# vim: set ts=4 et 
